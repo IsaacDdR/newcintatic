2ce6fbdba023f676eff7df43c559bfcafbc0f30a
Isaacddr <isaacovic00@gmail.com>
2021-08-15 14:20:33 -0500

    fix: changed language in FB chat plugin

--- plugins/vue-fb-customer-chat.js
+++ plugins/vue-fb-customer-chat.js
@@ -4,5 +4,5 @@ import VueFbCustomerChat from 'vue-fb-customer-chat'
 Vue.use(VueFbCustomerChat, {
   page_id: '109952554724805', //  change 'null' to your Facebook Page ID,
   theme_color: '#333333', // theme color in HEX
-  locale: 'en_US', // default 'en_US'
+  locale: 'es_LA', // default 'en_US'
 })
