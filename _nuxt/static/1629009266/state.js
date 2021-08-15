5228b2248903ed772b6daf4fbd744b94d9ce7e30
IsaacDdR <IsaacDdR@users.noreply.github.com>
2021-08-15 06:53:58 +0000

    deploy: a9f7289e7cae92f8f744c89b96ed166e490172a6

--- _nuxt/static/1629009266/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1629009266",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"90662598"}},__i18n:{langs:{}},_img:{"/_ipx/title-icon.png":"\u002F_nuxt\u002Fimage\u002F795feb.png"}}}(null,"\u002F"));
