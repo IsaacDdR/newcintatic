2b8979c9f9d5e3a4ac1d9b4c96df72c826ed3dc4
Isaacddr <isaacovic00@gmail.com>
2021-08-15 14:15:47 -0500

    chore: added FB chat plugin from vue plugin

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,7 +26,10 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ['@/assets/main.css'],
+  css: [
+    '@/assets/main.css',
+    { src: '~/plugins/vue-fb-customer-chat.js', ssr: false },
+  ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: ['@/plugins/aos.client'],
--- pages/index.vue
+++ pages/index.vue
@@ -2,34 +2,7 @@
   <div>
     <Nav data-aos="fade-down" data-aos-duration="1000" />
     <!--Hero -->
-    <!-- Messenger Chat Plugin Code -->
-    <div id="fb-root"></div>
 
-    <!-- Your Chat Plugin code -->
-    <div id="fb-customer-chat" class="fb-customerchat"></div>
-
-    <script>
-      var chatbox = document.getElementById('fb-customer-chat')
-      chatbox.setAttribute('page_id', '109952554724805')
-      chatbox.setAttribute('attribution', 'biz_inbox')
-
-      window.fbAsyncInit = function () {
-        FB.init({
-          xfbml: true,
-          version: 'v11.0',
-        })
-      }
-
-      ;(function (d, s, id) {
-        var js,
-          fjs = d.getElementsByTagName(s)[0]
-        if (d.getElementById(id)) return
-        js = d.createElement(s)
-        js.id = id
-        js.src = 'https://connect.facebook.net/es_LA/sdk/xfbml.customerchat.js'
-        fjs.parentNode.insertBefore(js, fjs)
-      })(document, 'script', 'facebook-jssdk')
-    </script>
     <div class="bg-gray-900">
       <div
         class="
