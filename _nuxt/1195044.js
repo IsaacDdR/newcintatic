b4159d363aba9ba352564a5163ae56b56b2cb34f
IsaacDdR <IsaacDdR@users.noreply.github.com>
2021-08-15 19:17:27 +0000

    deploy: 2b8979c9f9d5e3a4ac1d9b4c96df72c826ed3dc4

---
+++ _nuxt/1195044.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{333:function(o,t,e){"use strict";e.r(t),t.default={nav:{product:"Product"},main_header:{title:"Your landing page, fast at a low cost.",description:"Tell us who you are and we will give you a fast and modern site without loosing comfort"}}}}]);
