f75aa27d04a27ac967af35b4d2a5c34a35424bd5
IsaacDdR <IsaacDdR@users.noreply.github.com>
2021-08-30 05:13:50 +0000

    deploy: 67c7636a89a6f9ffc1d18ef4f21d776c1cbecd4b

--- _nuxt/static/1630292266/paquetes/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1630292266",layout:"default",error:a,serverRendered:true,routePath:"\u002Fpaquetes",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"27c02700"}},_img:{"/_ipx/title-icon.png":"\u002F_nuxt\u002Fimage\u002F795feb.png"}}}(null));
---
+++ _nuxt/static/1630300429/blog/como-te-ayuda-un-sitio/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1630300429",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fcomo-te-ayuda-un-sitio",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"9e902dc4"}},_img:{"/_ipx/title-icon.png":"\u002F_nuxt\u002Fimage\u002F795feb.png"}}}(null));
