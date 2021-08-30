973c17c3e81155a91ec6824512e7ab64a8311c51
IsaacDdR <IsaacDdR@users.noreply.github.com>
2021-08-30 02:57:47 +0000

    deploy: 1f1f92d9a94b3570fdd99c1f08c42ee6a09e8ea0

--- _nuxt/static/1630189122/blog/post1/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1630189122",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fpost1",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"1b4e6963"}},_img:{"/_ipx/title-icon.png":"\u002F_nuxt\u002Fimage\u002F795feb.png"}}}(null));
---
+++ _nuxt/static/1630292266/blog/post1/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1630292266",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fpost1",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"27c02700"}},_img:{"/_ipx/title-icon.png":"\u002F_nuxt\u002Fimage\u002F795feb.png"}}}(null));
