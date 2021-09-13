d77f921d569c51b711d3ea23990ba5a0d3c80d2b
Isaacddr <isaacovic00@gmail.com>
2021-09-13 11:54:24 -0500

    chore: changed FB ID

--- plugins/vue-fb-customer-chat.js
+++ plugins/vue-fb-customer-chat.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import VueFbCustomerChat from 'vue-fb-customer-chat'
 
 Vue.use(VueFbCustomerChat, {
-  page_id: '109952554724805', //  change 'null' to your Facebook Page ID,
+  page_id: '106070931823082', //  change 'null' to your Facebook Page ID,
   theme_color: '#333333', // theme color in HEX
   locale: 'es_LA', // default 'en_US'
 })
